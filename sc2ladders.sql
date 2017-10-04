7d5b47ca69e157f3a973d7efc066cd5abf82b9a0
cryptyc <crysison@googlemail.com>
2017-10-04 23:14:53 +0100

    New site database

--- sc2ladders.sql
+++ sc2ladders.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.1
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Sep 23, 2017 at 11:19 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.0.23
+-- Host: 127.0.0.1
+-- Generation Time: Oct 04, 2017 at 10:14 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +17,57 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `u364272929_sc2la`
+-- Database: `sc2ladders`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `botrequests`
+--
+
+CREATE TABLE `botrequests` (
+  `id` int(11) NOT NULL,
+  `FileLoc` varchar(128) DEFAULT NULL,
+  `DownloadLink` varchar(128) DEFAULT NULL,
+  `Comments` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loginattempts`
+--
+
+CREATE TABLE `loginattempts` (
+  `IP` varchar(20) NOT NULL,
+  `Attempts` int(11) NOT NULL,
+  `LastLogin` datetime NOT NULL,
+  `Username` varchar(65) DEFAULT NULL,
+  `ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` char(23) NOT NULL,
+  `username` varchar(65) NOT NULL DEFAULT '',
+  `password` varchar(65) NOT NULL DEFAULT '',
+  `email` varchar(65) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT '0',
+  `mod_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Joined` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Alias` varchar(128) DEFAULT NULL,
+  `Name` varchar(128) DEFAULT NULL,
+  `Avatar` varchar(128) DEFAULT NULL,
+  `Website` varchar(128) DEFAULT NULL,
+  `Github` varchar(128) DEFAULT NULL,
+  `ProfileVisible` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -31,9 +78,24 @@ SET time_zone = "+00:00";
 CREATE TABLE `participants` (
   `ID` int(11) NOT NULL,
   `Name` varchar(128) NOT NULL,
-  `Author` varchar(128) NOT NULL,
-  `Race` int(11) NOT NULL
+  `Author` varchar(100) NOT NULL,
+  `Race` int(11) NOT NULL,
+  `Verified` int(11) NOT NULL DEFAULT '0',
+  `Deactivated` tinyint(4) NOT NULL DEFAULT '0',
+  `Deleted` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resetrequests`
+--
+
+CREATE TABLE `resetrequests` (
+  `user` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `code` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -43,17 +105,70 @@ CREATE TABLE `participants` (
 
 CREATE TABLE `results` (
   `GameID` int(11) NOT NULL,
+  `Date` datetime DEFAULT NULL,
   `Bot1` int(11) NOT NULL,
   `Bot2` int(11) NOT NULL,
   `Map` varchar(64) NOT NULL,
-  `Date` datetime DEFAULT NULL,
   `Winner` int(11) NOT NULL,
-  `ReplayFile` varchar(128) DEFAULT NULL
+  `ReplayFile` varchar(128) DEFAULT NULL,
+  `SeasonId` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seasonids`
+--
+
+CREATE TABLE `seasonids` (
+  `id` int(11) NOT NULL,
+  `SeasonName` varchar(128) NOT NULL,
+  `Current` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seasons`
+--
+
+CREATE TABLE `seasons` (
+  `id` int(11) NOT NULL,
+  `EndDate` datetime DEFAULT NULL,
+  `Season` int(11) NOT NULL,
+  `BotId` int(11) NOT NULL,
+  `Author` varchar(50) NOT NULL,
+  `Matches` int(11) NOT NULL,
+  `Wins` int(11) NOT NULL,
+  `WinPct` float NOT NULL,
+  `Position` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `botrequests`
+--
+ALTER TABLE `botrequests`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `id_2` (`id`);
+
+--
+-- Indexes for table `loginattempts`
+--
+ALTER TABLE `loginattempts`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`);
 
 --
 -- Indexes for table `participants`
@@ -62,15 +177,38 @@ ALTER TABLE `participants`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `resetrequests`
+--
+ALTER TABLE `resetrequests`
+  ADD PRIMARY KEY (`code`);
+
+--
 -- Indexes for table `results`
 --
 ALTER TABLE `results`
   ADD PRIMARY KEY (`GameID`);
 
 --
+-- Indexes for table `seasonids`
+--
+ALTER TABLE `seasonids`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seasons`
+--
+ALTER TABLE `seasons`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `loginattempts`
+--
+ALTER TABLE `loginattempts`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `participants`
 --
@@ -80,8 +218,17 @@ ALTER TABLE `participants`
 -- AUTO_INCREMENT for table `results`
 --
 ALTER TABLE `results`
-  MODIFY `GameID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=192;COMMIT;
-
+  MODIFY `GameID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=309;
+--
+-- AUTO_INCREMENT for table `seasonids`
+--
+ALTER TABLE `seasonids`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `seasons`
+--
+ALTER TABLE `seasons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
