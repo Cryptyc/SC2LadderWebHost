79d8e75d1dcda66868f84e8932d983ba5d466a77
IanGallacher <gallachergithub@outlook.com>
2017-10-14 15:05:45 -0700

    Adds untracked file

---
+++ js/common.js
@@ -0,0 +1,35 @@
+// Most of this code is from here: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table
+function bubbleSortTable(tableID, tableElementIndex) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById(tableID);
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[tableElementIndex];
+      y = rows[i + 1].getElementsByTagName("TD")[tableElementIndex];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch= true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
