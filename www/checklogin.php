d974d519cd66676855b3772612f369b77f5b6dab
Mitja <mitja@mitja.cc>
2017-10-31 17:05:23 +0100

    deduplicated checklogin code

--- www/checklogin.php
+++ www/checklogin.php
@@ -19,40 +19,24 @@ $conf = new GlobalConf;
 $lastAttempt = checkAttempts($username);
 $max_attempts = $conf->max_attempts;
 
-
-//First Attempt
 if ($lastAttempt['lastlogin'] == '') {
-
+    //First Attempt
     $lastlogin = 'never';
     $loginCtl->insertAttempt($username);
-    $response = $loginCtl->checkLogin($username, $password);
-
 } elseif ($lastAttempt['attempts'] >= $max_attempts) {
-
     //Exceeded max attempts
     $loginCtl->updateAttempts($username);
-    $response = $loginCtl->checkLogin($username, $password);
+}
 
-} else {
-
-    $response = $loginCtl->checkLogin($username, $password);
-
-};
+$response = $loginCtl->checkLogin($username, $password);
 
 if ($lastAttempt['attempts'] < $max_attempts && $response != 'true') {
-
     $loginCtl->updateAttempts($username);
-    $resp = new RespObj($username, $response);
-    $jsonResp = json_encode($resp);
-    echo $jsonResp;
-
-} else {
-
-    $resp = new RespObj($username, $response);
-    $jsonResp = json_encode($resp);
-    echo $jsonResp;
-
 }
+
+$resp = new RespObj($username, $response);
+$jsonResp = json_encode($resp);
+echo $jsonResp;
 
 unset($resp, $jsonResp);
 ob_end_flush();
